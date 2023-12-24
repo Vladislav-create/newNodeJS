ede202efda4600f5d276692c715a85180b491b54
Vlad <29-vinitskiy-vo@mail.ru>
2023-12-24 13:45:52 +0700

    hw2

---
+++ hw2/library_for_creating_product_cards/README.md
@@ -0,0 +1,2 @@
+# library_for_creating_product_cards
+library for generating the required number of product cards for tests
---
+++ hw2/test_use_library/index.js
@@ -0,0 +1,5 @@
+const creatingProductCardsLib = require('library_for_creating_product_cards')
+
+const cart = creatingProductCardsLib.creatorObject('Шоколадка', 'Кондитерское изделие', 'Сделано из натурального бельгийского шоколада')
+
+console.log(cart);
